a56b36dd148ec0c2c486a740cb915d29f286c3e8
Nikhil <ennsharma@berkeley.edu>
2018-12-19 15:05:14 -0800

    Implemented basic blackjack with hit and stand

--- gym-blackjack/gym-blackjack/envs/blackjack_env.py
+++ gym-blackjack/gym-blackjack/envs/blackjack_env.py
@@ -1,4 +1,6 @@
 import gym
+import numpy as np
+
 from gym import error, spaces, utils
 from gym.utils import seeding
 
@@ -6,13 +8,45 @@ class BlackjackEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    pass
+    self.action_map = {
+    	0 : self._hit,
+    	1 : self._stand,
+    	2 : self._double,
+    	3 : self._split,
+    	4 : self._surrender
+    }
+    self.card_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    self.reset()
 
   def step(self, action):
-    pass
+    action_func = self.action_map[action]
+    obs, rew, done = action_func()
+    return obs, rew, done, {}
 
   def reset(self):
-    pass
+    self.total = 0
+    self._hit()
+    self._hit()
 
   def render(self, mode='human', close=False):
     pass
+
+  def _hit():
+  	self.total += self.card_values[np.random.randint(0, 13)]
+  	if self.total > 21:
+  		return self.total, -1, True
+  	return self.total, 0, True
+  
+  def _stand():
+  	return self.total, self.total, True
+
+  def _double():
+  	pass
+
+  def _split():
+  	pass
+
+  def _surrender():
+  	pass
+
+
